d35b584094d1a587b24ef1d64fa271b55ad39d15
kazuvin <miurakazuya@Kazuya.local>
2024-04-16 23:53:19 +0900

    Update: path alias 用 plugin の追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 });
